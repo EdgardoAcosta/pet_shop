2a2d4b8ca816dabd974fe5fb423dd7a64ecb9c8b
MaNuEl33 <manuelharo1994@gmail.com>
2017-07-08 20:52:58 -0300

    Registro cliente 100%

--- routes/admin.js
+++ routes/admin.js
@@ -14,12 +14,64 @@ router.get('/register/admin', (req, res, next)=> {
         res.redirect('/');
 });
 
+router.get('/register/client', (req, res, next) => {
+    let sess = req.session;
+    if(sess.Type == 'admin')
+        res.render('register_client', {title: 'Cadastrar Cliente', sess: sess});
+    else
+        res.redirect('/');
+});
+
 router.get('/', (req, res, next)=> {
     let sess = req.session;
     if (sess.Type == 'admin')
         res.redirect('/admin/register/admin');
     else
         res.redirect('/');
+});
+
+router.post('/register_client', (req, res, next) => {
+    var fields = [];
+    var form = new formidable.IncomingForm();
+
+    form.uploadDir = 'public/images/Users/';
+
+    form.on('field', (field, value)=> {
+        fields[field] = value;
+    });
+    form.on('file', (name, file)=> {
+        fields[name] = file;
+        fs.rename(file.path, form.uploadDir + "/" + file.name);
+    });
+
+    form.parse(req);
+
+    form.on('end', function () {
+
+        conn.view('all_users', 'get_clients', {key: fields['id']}, (err, body)=> {
+            if (!err) {
+                if (body.rows.length > 0) {
+                    console.log('Cliente ya registrado');
+                    res.redirect('/admin');
+                } else {
+                    conn.insert({
+                        Id_User: fields['id'],
+                        Type: 'admin',
+                        Name: fields['nome'],
+                        Password: fields['pass'],
+                        Phone: fields['telefone'],
+                        Email: fields['email'],
+                        Adress: fields['endereco'],
+                        Photo: '/images/Users/' + fields['photo']
+                    }, (err, body)=> {
+                        if (!err) {
+                            res.redirect('/admin/register/client');
+                        }                
+                    })
+                }
+            }
+        });
+    });
 });
 
 router.post('/register_admin', (req, res, next) => {
@@ -40,7 +92,7 @@ router.post('/register_admin', (req, res, next) => {
 
     form.on('end', function () {
 
-        conn.view('admins', 'get_admins', {key: fields['id']}, (err, body)=> {
+        conn.view('all_users', 'get_admins', {key: fields['id']}, (err, body)=> {
             if (!err) {
                 if (body.rows.length > 0) {
                     console.log('Administrador ya registrado');
