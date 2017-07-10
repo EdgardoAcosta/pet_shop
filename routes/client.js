eb9ea26373d6b857db7806b5235cf2a9a97572d8
MaNuEl33 <manuelharo1994@gmail.com>
2017-07-09 23:37:55 -0300

    Proyecto listo 90%

--- routes/client.js
+++ routes/client.js
@@ -14,6 +14,19 @@ router.get('/', (req, res, next)=> {
         res.redirect('/');
 });
 
+router.get('/edit', (req, res, next) => {
+    let sess = req.session;
+    if (sess.Type == 'user') {
+        let idUser = req.query.idUser;
+        conn.view('all_users', 'get_clients', {key: idUser}, (err, body) => {
+            let user = body.rows[0];
+            console.log(body.rows[0]);
+            res.render('edit_user', {title: 'Editar Usuario', user: user, sess: sess});
+        });        
+    } else 
+        res.redirect('/');
+});
+
 router.get('/pets', (req, res, next) => {
     let sess = req.session;
     if (sess.Type == 'user') {
@@ -66,6 +79,41 @@ router.get('/pets/delete', (req, res, next) => {
         });
     } else 
         res.redirect('/');
+});
+
+router.post('/edit_user', (req, res, next) => {
+    var fields = [];
+    var form = new formidable.IncomingForm();
+
+    form.uploadDir = 'public/images/Users/';
+
+    form.on('field', (field, value)=> {
+        fields[field] = value;
+    });
+    form.on('file', (name, file)=> {
+        fields[name] = file;
+        fs.rename(file.path, form.uploadDir + "/" + file.name);
+    });
+
+    form.parse(req);
+
+    form.on('end', function () {
+        conn.insert({
+            _rev: fields['rev'],
+            Id_User: fields['idUser'],
+            Type: 'user',
+            Name: fields['nome'],
+            Email: fields['email'],
+            Password: fields['pass'],
+            Phone: fields['telefone'],
+            Photo: '/images/Pets/' + fields['photo'],
+            Adress: fields['endereco'],
+        }, fields['id'], (err, body)=> {
+            if (!err) {
+                res.redirect('/client/pets?idUser=' + fields['idUser']);
+            }                
+        });
+    });
 });
 
 router.post('/edit_pet', (req, res, next) => {
@@ -142,204 +190,5 @@ router.post('/add_pet', (req, res, next) => {
         });
     });
 });
-
-router.get('/manage/products/delete', (req, res, next) => {
-    let sess = req.session;
-    if (sess.Type == 'admin') {
-        let id = req.query.id;
-        let rev = req.query.rev;
-        conn.destroy(id, rev, (err, body) => {
-            res.redirect('/admin/manage/products');
-        });
-    } else {
-        res.redirect('/');
-    }
-});
-
-router.post('/edit_product', (req, res, next) => {
-    var fields = [];
-    var form = new formidable.IncomingForm();
-
-    form.uploadDir = 'public/images/Categories/';
-
-    form.on('field', (field, value)=> {
-        fields[field] = value;
-    });
-    form.on('file', (name, file)=> {
-        fields[name] = file;
-        fs.rename(file.path, form.uploadDir + "/" + file.name);
-    });
-
-    form.parse(req);
-
-    form.on('end', function () {
-        conn.insert({
-            _rev: fields['rev'],
-            Id_Prod: fields['idProd'],
-            Type: fields['tipo'],
-            Name: fields['nome'],
-            Description: fields['descricao'],
-            Price: fields['preco'],
-            Stock: fields['quantidade'],
-            Photo: '/images/Categories/' + fields['photo']
-        }, fields['id'], (err, body)=> {
-            if (!err) {
-                res.redirect('/admin/manage/products');
-            }                
-        });
-    });
-});
-
-router.post('/register_service', (req, res, next) => {
-    var fields = [];
-    var form = new formidable.IncomingForm();
-
-    form.uploadDir = 'public/images/Services/';
-
-    form.on('field', (field, value)=> {
-        fields[field] = value;
-    });
-    form.on('file', (name, file)=> {
-        fields[name] = file;
-        fs.rename(file.path, form.uploadDir + "/" + file.name);
-    });
-
-    form.parse(req);
-
-    form.on('end', function () {
-        conn.insert({
-            Id_Service: fields['id'],
-            Name: fields['nome'],
-            Description: fields['descricao'],
-            Price: fields['preco'],
-            Photo: '/images/Services/' + fields['photo']
-        }, (err, body)=> {
-            if (!err) {
-                res.redirect('/admin/register/service');
-            }                
-        });
-    });
-});
-
-router.post('/register_product', (req, res, next) => {
-    var fields = [];
-    var form = new formidable.IncomingForm();
-
-    form.uploadDir = 'public/images/Categories/';
-
-    form.on('field', (field, value)=> {
-        fields[field] = value;
-    });
-    form.on('file', (name, file)=> {
-        fields[name] = file;
-        fs.rename(file.path, form.uploadDir + "/" + file.name);
-    });
-
-    form.parse(req);
-
-    form.on('end', function () {
-        conn.insert({
-            Id_Prod: fields['id'],
-            Type: fields['tipo'],
-            Name: fields['nome'],
-            Description: fields['descricao'],
-            Price: fields['preco'],
-            Stock: fields['quantidade'],
-            Photo: '/images/Categories/' + fields['photo']
-        }, (err, body)=> {
-            if (!err) {
-                res.redirect('/admin/register/product');
-            }                
-        });
-    });
-});
-
-router.post('/register_client', (req, res, next) => {
-    var fields = [];
-    var form = new formidable.IncomingForm();
-
-    form.uploadDir = 'public/images/Users/';
-
-    form.on('field', (field, value)=> {
-        fields[field] = value;
-    });
-    form.on('file', (name, file)=> {
-        fields[name] = file;
-        fs.rename(file.path, form.uploadDir + "/" + file.name);
-    });
-
-    form.parse(req);
-
-    form.on('end', function () {
-
-        conn.view('all_users', 'get_clients', {key: fields['id']}, (err, body)=> {
-            if (!err) {
-                if (body.rows.length > 0) {
-                    console.log('Cliente ya registrado');
-                    res.redirect('/admin');
-                } else {
-                    conn.insert({
-                        Id_User: fields['id'],
-                        Type: 'admin',
-                        Name: fields['nome'],
-                        Password: fields['pass'],
-                        Phone: fields['telefone'],
-                        Email: fields['email'],
-                        Adress: fields['endereco'],
-                        Photo: '/images/Users/' + fields['photo']
-                    }, (err, body)=> {
-                        if (!err) {
-                            res.redirect('/admin/register/client');
-                        }                
-                    })
-                }
-            }
-        });
-    });
-});
-
-router.post('/register_admin', (req, res, next) => {
-    var fields = [];
-    var form = new formidable.IncomingForm();
-
-    form.uploadDir = 'public/images/Users/';
-
-    form.on('field', (field, value)=> {
-        fields[field] = value;
-    });
-    form.on('file', (name, file)=> {
-        fields[name] = file;
-        fs.rename(file.path, form.uploadDir + "/" + file.name);
-    });
-
-    form.parse(req);
-
-    form.on('end', function () {
-
-        conn.view('all_users', 'get_admins', {key: fields['id']}, (err, body)=> {
-            if (!err) {
-                if (body.rows.length > 0) {
-                    console.log('Administrador ya registrado');
-                    res.redirect('/admin');
-                } else {
-                    conn.insert({
-                        Id_User: fields['id'],
-                        Type: 'admin',
-                        Name: fields['nome'],
-                        Password: fields['pass'],
-                        Phone: fields['telefone'],
-                        Email: fields['email'],
-                        Photo: '/images/Users/' + fields['photo']
-                    }, (err, body)=> {
-                        if (!err) {
-                            res.redirect('/admin');
-                        }                
-                    })
-                }
-            }
-        });
-    });
-});
-
 
 module.exports = router;
